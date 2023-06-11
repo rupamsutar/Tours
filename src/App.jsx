6863b4d0fff3f9c765562692eb0faeb52e014f29
Rupam Sutar <rupamsutar281998@gmail.com>
2023-06-11 10:31:47 +0530

    update 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async() => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      console.log(tours);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  useEffect(() => {
+    fetchTours();
+  },[])
+
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+
   return <h2>Tours Starter</h2>;
 };
 export default App;
